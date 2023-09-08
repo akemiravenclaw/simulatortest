3ddc719d213b92afd12366aa48903a46a35b207f
AkemiRavenclaw <emcrg@icloud.com>
2023-09-07 20:25:14 -0500

    arrays, filters and prompts added

--- miarchivo.js
+++ miarchivo.js
@@ -2,34 +2,105 @@ document.addEventListener("DOMContentLoaded", function() {
     const procesoSelector = document.getElementById("procesoSelector");
     const formularioDiv = document.getElementById("formulario");
     const resultadoDiv = document.getElementById("resultado");
-    
+
+    // Función para capturar entradas mediante prompt()
+    function capturarEntradas() {
+        const cantidadLibros = parseFloat(prompt("Ingrese la cantidad de libros:"));
+        const precioUnitario = parseFloat(prompt("Ingrese el precio unitario por libro:"));
+        if (isNaN(cantidadLibros) || isNaN(precioUnitario)) {
+            resultadoDiv.innerHTML = "Por favor, ingrese valores válidos.";
+            return;
+        }
+        const costoTotal = cantidadLibros * precioUnitario;
+        resultadoDiv.innerHTML = `El costo total es: $${costoTotal.toFixed(2)}`;
+    }
+
+    // Función para calcular pagos en cuotas
+    function calcularCuotas() {
+        const montoTotal = parseFloat(document.getElementById("montoTotal").value);
+        const cuotas = parseInt(document.getElementById("cuotas").value);
+        if (isNaN(montoTotal) || isNaN(cuotas)) {
+            resultadoDiv.innerHTML = "Por favor, ingrese valores válidos.";
+            return;
+        }
+        const pagoPorCuota = montoTotal / cuotas;
+        resultadoDiv.innerHTML = `Pago por cuota: $${pagoPorCuota.toFixed(2)}`;
+    }
+
+    // Función para calcular el valor final
+    function calcularValorFinal() {
+        const valorInicial = parseFloat(document.getElementById("valorInicial").value);
+        const impuestos = parseFloat(document.getElementById("impuestos").value);
+        const descuentos = parseFloat(document.getElementById("descuentos").value);
+        if (isNaN(valorInicial) || isNaN(impuestos) || isNaN(descuentos)) {
+            resultadoDiv.innerHTML = "Por favor, ingrese valores válidos.";
+            return;
+        }
+        const impuestosAmount = (impuestos / 100) * valorInicial;
+        const descuentosAmount = (descuentos / 100) * valorInicial;
+        const valorFinal = valorInicial + impuestosAmount - descuentosAmount;
+        resultadoDiv.innerHTML = `Valor final: $${valorFinal.toFixed(2)}`;
+    }
+
+    // Lista de libros
+    const libros = [
+        {
+            nombre: "Herejes De Dune (Dune 5)",
+            autor: "FRANK HERBERT",
+            precio: 15.99,
+        },
+        {
+            nombre: "Ocho horas para enamorarse",
+            autor: "LIA LOUIS",
+            precio: 12.49,
+        },
+        {
+            nombre: "La Chica No Olvida",
+            autor: "IRENE X",
+            precio: 18.99,
+        },
+        // Agregar más libros en el futuro
+    ];
+
+    // Función para buscar un libro por nombre y mostrar su información
+    function buscarLibroPorNombre(nombre) {
+        const libroEncontrado = libros.find(libro => libro.nombre.toLowerCase() === nombre.toLowerCase());
+        if (libroEncontrado) {
+            resultadoDiv.innerHTML = `Nombre: ${libroEncontrado.nombre}<br>Autor: ${libroEncontrado.autor}<br>Precio: $${libroEncontrado.precio.toFixed(2)}`;
+        } else {
+            resultadoDiv.innerHTML = "Libro no encontrado.";
+        }
+    }
+
+    // Función para filtrar libros por precio
+    function filtrarLibrosPorPrecio(precioMaximo) {
+        const librosFiltrados = libros.filter(libro => libro.precio <= precioMaximo);
+        if (librosFiltrados.length > 0) {
+            resultadoDiv.innerHTML = "Libros encontrados:<br>";
+            librosFiltrados.forEach(libro => {
+                resultadoDiv.innerHTML += `${libro.nombre} - Autor: ${libro.autor} - Precio: $${libro.precio.toFixed(2)}<br>`;
+            });
+        } else {
+            resultadoDiv.innerHTML = "No se encontraron libros dentro del precio máximo.";
+        }
+    }
+
+    // Función para mostrar todos los productos disponibles
+    function mostrarProductosDisponibles() {
+        resultadoDiv.innerHTML = "Productos disponibles:<br>";
+        libros.forEach(libro => {
+            resultadoDiv.innerHTML += `${libro.nombre} - Autor: ${libro.autor} - Precio: $${libro.precio.toFixed(2)}<br>`;
+        });
+    }
+
+    // Manejar cambios en el selector de procesos
     procesoSelector.addEventListener("change", function() {
         const procesoSeleccionado = procesoSelector.value;
-        formularioDiv.innerHTML = ""; // funciona para limpiar el formulario
-        resultadoDiv.innerHTML = ""; // funciona para limpiar el resultado
-        
-        if (procesoSeleccionado === "calcularCosto") {
-            formularioDiv.innerHTML = `
-                <label for="cantidadProductos">Cantidad de productos:</label>
-                <input type="number" id="cantidadProductos" step="1" min="0">
-                <br>
-                <label for="precioUnitario">Precio unitario:</label>
-                <input type="number" id="precioUnitario" step="0.01" min="0">
-                <br>
-                <button id="calcularCostoBtn">Calcular Costo</button>
-            `;
-            
-            const calcularCostoBtn = document.getElementById("calcularCostoBtn");
-            calcularCostoBtn.addEventListener("click", function() {
-                const cantidadProductos = parseFloat(document.getElementById("cantidadProductos").value);
-                const precioUnitario = parseFloat(document.getElementById("precioUnitario").value);
-                if (isNaN(cantidadProductos) || isNaN(precioUnitario)) {
-                    resultadoDiv.innerHTML = "Por favor, ingrese valores válidos.";
-                    return;
-                }
-                const costoTotal = cantidadProductos * precioUnitario;
-                resultadoDiv.innerHTML = `El costo total es: $${costoTotal.toFixed(2)}`;
-            });
+        formularioDiv.innerHTML = ""; // Funciona para limpiar el formulario
+        resultadoDiv.innerHTML = ""; // Funciona para limpiar el resultado
+
+        if (procesoSeleccionado === "capturarEntradas") {
+            capturarEntradas();
         } else if (procesoSeleccionado === "calcularCuotas") {
             formularioDiv.innerHTML = `
                 <label for="montoTotal">Monto total:</label>
@@ -40,17 +111,9 @@ document.addEventListener("DOMContentLoaded", function() {
                 <br>
                 <button id="calcularCuotasBtn">Calcular Cuotas</button>
             `;
-            
             const calcularCuotasBtn = document.getElementById("calcularCuotasBtn");
             calcularCuotasBtn.addEventListener("click", function() {
-                const montoTotal = parseFloat(document.getElementById("montoTotal").value);
-                const cuotas = parseInt(document.getElementById("cuotas").value);
-                if (isNaN(montoTotal) || isNaN(cuotas)) {
-                    resultadoDiv.innerHTML = "Por favor, ingrese valores válidos.";
-                    return;
-                }
-                const pagoPorCuota = montoTotal / cuotas;
-                resultadoDiv.innerHTML = `Pago por cuota: $${pagoPorCuota.toFixed(2)}`;
+                calcularCuotas(); // Llamando a la función para calcular pagos en cuotas
             });
         } else if (procesoSeleccionado === "calcularValorFinal") {
             formularioDiv.innerHTML = `
@@ -65,23 +128,41 @@ document.addEventListener("DOMContentLoaded", function() {
                 <br>
                 <button id="calcularValorFinalBtn">Calcular Valor Final</button>
             `;
-            
             const calcularValorFinalBtn = document.getElementById("calcularValorFinalBtn");
             calcularValorFinalBtn.addEventListener("click", function() {
-                const valorInicial = parseFloat(document.getElementById("valorInicial").value);
-                const impuestos = parseFloat(document.getElementById("impuestos").value);
-                const descuentos = parseFloat(document.getElementById("descuentos").value);
-                if (isNaN(valorInicial) || isNaN(impuestos) || isNaN(descuentos)) {
-                    resultadoDiv.innerHTML = "Por favor, ingrese valores válidos.";
-                    return;
-                }
-                const impuestosAmount = (impuestos / 100) * valorInicial;
-                const descuentosAmount = (descuentos / 100) * valorInicial;
-                const valorFinal = valorInicial + impuestosAmount - descuentosAmount;
-                resultadoDiv.innerHTML = `Valor final: $${valorFinal.toFixed(2)}`;
+                calcularValorFinal(); // Llamando a la función para calcular el valor final
             });
+        } else if (procesoSeleccionado === "buscarProducto") {
+            formularioDiv.innerHTML = `
+                <label for="nombreLibro">Nombre del libro:</label>
+                <input type="text" id="nombreLibro">
+                <br>
+                <button id="buscarLibroBtn">Buscar Libro</button>
+            `;
+            const buscarLibroBtn = document.getElementById("buscarLibroBtn");
+            buscarLibroBtn.addEventListener("click", function() {
+                const nombreLibro = document.getElementById("nombreLibro").value;
+                buscarLibroPorNombre(nombreLibro); // Llamando a la función para buscar un libro por nombre
+            });
+        } else if (procesoSeleccionado === "filtrarProductos") {
+            formularioDiv.innerHTML = `
+                <label for="precioMaximo">Precio máximo:</label>
+                <input type="number" id="precioMaximo" step="0.01" min="0">
+                <br>
+                <button id="filtrarLibrosBtn">Filtrar Libros</button>
+            `;
+            const filtrarLibrosBtn = document.getElementById("filtrarLibrosBtn");
+            filtrarLibrosBtn.addEventListener("click", function() {
+                const precioMaximo = parseFloat(document.getElementById("precioMaximo").value);
+                if (!isNaN(precioMaximo)) {
+                    filtrarLibrosPorPrecio(precioMaximo); // Llamando a la función para filtrar libros por precio
+                } else {
+                    resultadoDiv.innerHTML = "Por favor, ingrese un precio máximo válido.";
+                }
+            });
+        } else if (procesoSeleccionado === "verProductosDisponibles") {
+            mostrarProductosDisponibles(); // Llamando a la función para mostrar todos los productos disponibles
         } else {
-            // Manejar otros procesos
             formularioDiv.innerHTML = `Proceso no implementado.`;
         }
     });
